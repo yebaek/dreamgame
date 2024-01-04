d3703c9728f6f6134d8b47bb1f019720de566d2c
김예백 <yebaek02@naver.com>
2024-01-04 20:49:12 +0900

    1/4

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// Fetch the items from the JSON file (jsom 파일로부터 아이템을 동적으로 가져오기)
+function loadItems() {
+  return fetch('data/data.json')
+    .then(response => response.json())
+    .then(json => json.items);
+}
+
+// Update the list with the given items
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  const html = items.map(item => createHTMLString(item)).join('');
+  container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+  return `
+  <li class="item">
+    <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+    <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+// main
+loadItems()
+.then(items => {
+  displayItems(items);
+  // setEventListeners(items);
+})
+.catch(console.log)

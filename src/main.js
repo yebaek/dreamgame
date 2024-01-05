af6018a56760fcd3510e2ede76d968571c4b29d4
김예백 <yebaek02@naver.com>
2024-01-05 17:33:16 +0900

    1/5

--- src/main.js
+++ src/main.js
@@ -8,7 +8,6 @@ function loadItems() {
 // Update the list with the given items
 function displayItems(items) {
   const container = document.querySelector('.items');
-  const html = items.map(item => createHTMLString(item)).join('');
   container.innerHTML = items.map(item => createHTMLString(item)).join('');
 }
 
@@ -21,10 +20,30 @@ function createHTMLString(item) {
   </li>
   `;
 }
+
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  
+  displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
 .then(items => {
   displayItems(items);
-  // setEventListeners(items);
+  setEventListeners(items);
 })
 .catch(console.log)
